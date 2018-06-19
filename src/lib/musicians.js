f62aca2e8291d91594f3901c54e14f81359ec348
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-19 15:12:49 +0200

    work in progress

--- src/lib/musicians.js
+++ src/lib/musicians.js
@@ -5,7 +5,8 @@ const musicians  = [
   age: 30,
   instrument: 'base guitar',
   description: 'I am a very passioned beginner. I also love puppies.',
-  skillLevel: 1
+  skillLevel: 2,
+  genre: rock,
   picture: ./logo.svg
 },
 
@@ -15,7 +16,72 @@ const musicians  = [
   age: 25,
   instrument: 'violin',
   description: 'I love the violin, but still learning',
-  skillLevel: 1
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Max',
+  age: 27,
+  instrument: 'electric guitar',
+  description: 'Let s rock and roll!',
+  skillLevel: 1,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Alex',
+  age: 30,
+  instrument: 'Singer',
+  description: 'Singing under the shower',
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Harry',
+  age: 42,
+  instrument: 'base gitar',
+  description: 'Music and beers is all I need',
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Hank',
+  age: 18,
+  instrument: 'piano',
+  description: 'Just starting and looking for friends',
+  skillLevel: 3,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Mike',
+  age: 18,
+  instrument: 'base gitar',
+  description: 'Crazy music mike they say',
+  skillLevel: 2,
+  genre: rock,
+  picture: ./logo.svg
+}
+,
+
+{
+  id: 1,
+  name: 'Mose',
+  age: 25,
+  instrument: 'violin',
+  description: 'Classical music',
+  skillLevel: 1,
   picture: ./logo.svg
 },
 
@@ -35,7 +101,8 @@ const musicians  = [
   age: 30,
   instrument: 'Singer',
   description: 'Singing under the shower',
-  skillLevel: 1
+  skillLevel: 1,
+  genre: rock,
   picture: ./logo.svg
 },
 
@@ -45,7 +112,8 @@ const musicians  = [
   age: 42,
   instrument: 'base gitar',
   description: 'Music and beers is all I need',
-  skillLevel: 1
+  skillLevel: 1,
+  genre: rock,
   picture: ./logo.svg
 },
 
@@ -55,7 +123,7 @@ const musicians  = [
   age: 18,
   instrument: 'piano',
   description: 'Just starting and looking for friends',
-  skillLevel: 1
+  skillLevel: 3,
   picture: ./logo.svg
 },
 
@@ -65,7 +133,73 @@ const musicians  = [
   age: 18,
   instrument: 'base gitar',
   description: 'Crazy music mike they say',
-  skillLevel: 1
+  skillLevel: 2,
+  picture: ./logo.svg
+}
+,
+
+{
+  id: 1,
+  name: 'Amanda',
+  age: 25,
+  instrument: 'violin',
+  description: 'I love the violin, but still learning',
+  skillLevel: 1,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Maxime',
+  age: 27,
+  instrument: 'electric guitar',
+  description: 'Let s rock and roll!',
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Ally',
+  age: 30,
+  instrument: 'Singer',
+  description: 'Singing under the shower',
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Rietje',
+  age: 42,
+  instrument: 'base gitar',
+  description: 'Music and beers is all I need',
+  skillLevel: 1,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Hank',
+  age: 18,
+  instrument: 'piano',
+  description: 'Just starting and looking for friends',
+  skillLevel: 3,
+  genre: rock,
+  picture: ./logo.svg
+},
+
+{
+  id: 1,
+  name: 'Tasos',
+  age: 18,
+  instrument: 'base gitar',
+  description: 'Crazy music mike they say',
+  skillLevel: 2,
+  genre: rock,
   picture: ./logo.svg
 }
 ]
