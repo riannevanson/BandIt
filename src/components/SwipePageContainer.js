356d5c896bc7d971a6da8b7fd70f32c1a0ad79bf
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-20 17:31:47 +0200

    for alice

--- src/components/SwipePageContainer.js
+++ src/components/SwipePageContainer.js
@@ -2,21 +2,23 @@ import * as React from "react";
 import { connect } from "react-redux";
 import Card from "./Card";
 import Buttons from "./Buttons";
-import MatchContainer from "./MatchContainer"
-
+import MatchContainer from "./MatchContainer";
 
 class SwipePageContainer extends React.PureComponent {
   componentDidMount() {}
   render() {
     return (
       <div>
-        <div class="swipePageContainer">
-          <Card />
-          <Buttons />
-          <MatchContainer />
-
+        <div className="swipePageContainer">
+          <div className="cardAndButtons">
+            <Card />
+            <Buttons />
+          </div>
+          <div className="MatchContainer">
+            <MatchContainer />
+          </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
