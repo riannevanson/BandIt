d950ea1eeb7771f7109265825af3a5335db97051
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-20 12:27:40 +0200

    made random card yeeaah

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,27 +5,33 @@ import {newCard} from '../actions/swipe'
 
 class Card extends React.PureComponent {
 
-activeCard = () => {
-    const randomCard = logic.randomCard()
-    return this.props.newCard(randomCard)
+  activeCard = () => {
+     this.props.newCard(logic.randomCard())
   }
 
+  componentDidMount(){
+    console.log('I get called!')
+    this.props.newCard(logic.randomCard())
+  }
 
   render() {
     console.log(this.props);
     return (
       <div>
         {" "}
-        {this.props.musicians.map(musician => {
+        {/* {this.props.musicians.map(musician => {
           return (
             <div className="card">
               <img className="cardImage" src={musician.picture}/>
               <h2>{musician.name}</h2>
               <h4>{musician.description}</h4>
-              <button onClick = {this.activeCard}>Next Match</button>
+
             </div>
           );
-        })}
+        })} */}
+        <h1> {this.props.randomUser.name}</h1>
+
+    <button onClick = {this.activeCard}>Next Match</button>
       </div>
     );
   }
@@ -33,7 +39,8 @@ activeCard = () => {
 
 const mapStateToProps = function(state) {
   return {
-    musicians: state.musicians
+    musicians: state.musicians,
+    randomUser: state.newCard
   };
 };
 
